db7ebbb830def7457688b4b90e06c13d49588794
Lakshaya Inani <lakshaya.inani@gmail.com>
2022-10-11 21:35:00 +0530

    added Insertion sort in python

---
+++ DSA/Python/Sorting/InsertionSort.py
@@ -0,0 +1,12 @@
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        key_item = array[i]
+        j = i - 1
+        while j >= 0 and array[j] > key_item:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = key_item
+    return array
+
+a = eval(input("enter a list with [] sep by ,"))
+print(insertion_sort(a))
